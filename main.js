5e317b53072deac8e16befe2a1cfd45c3f43c5aa
Cyber-Adi <76634267+Cyber-Adi@users.noreply.github.com>
2020-12-26 19:18:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function one() {
+  window.alert("The Great Wall of China, famously created to protect the Chinese Empire from invading Mongol hordes, is a segmented series of multiple walls connected by distinctive square shaped guard towers, spanning about 4000 miles. It holds the title of the world’s longest manmade structure, and is testament to wall building techniques found as early as the 7th century B.C., when its dynasty-spanning construction began.")
+}
+function two() {
+  window.alert("Built in 1931 of reinforced concrete and soapstone, the 130 foot tall Christ the Redeemer statue, depicting Jesus Christ looming over the city of Rio de Janeiro with welcoming outstretched arms, remains Brazil’s most instantly recognizable landmark. Designed by Heitor da Silva Costa, this huge statue on Corcovado Mountain cost around $250,000 to build, most of which was given in donation. Though the statue has been damaged by lightning, weather and other natural conditions over the years, restoration efforts have kept it largely intact.")
+}
+function three() {
+  window.alert("Built in the 15th century by the Incas of Peru, the pyramid city of Machu Picchu was abandoned only a century after its creation during the Spanish Conquest. Since conquistadors never discovered this Lost City of the Incas, it remained largely untouched until its rediscovery in 1911, making it a uniquely preserved and important historical artifact. This lost city features many classic Incan building techniques and has taught us a great deal of what we know about Incan civilization today. A century of careful restoration on many of the buildings allows tourists to see what many of the structures originally looked like, and restoration efforts continue to this day.")
+}
+function four() {
+  window.alert("Originally built by the Maya civilization as early as 600 AD, Chichen Itza was an important ancient city featuring several famous pyramids and other architectural styles. The ruins of the city are federal property and the invaluable cultural site is maintained by Mexico’s National Institute of Anthropology and History, hosting well over 1 million tourists per year.")
+}
+function five() {
+  window.alert("One of the original 7 Wonders of the Ancient World, the Roman Coliseum is still the largest amphitheater in the world, holding an estimated audience of between 50,000 and 80,000 spectators at once. It represents one of the greatest architectural and engineering feats ever accomplished by mankind, and was built in a span of just 10 years, reaching completion in 80 A.D. Partially ruined by natural disasters and stone robbers, it remains one of the most iconic symbols of Rome and of Italy.")
+}
+function six() {
+  window.alert("Originally built as a mausoleum for the beloved wife of a Mughal emperor, the Taj Mahal is possibly the greatest example of Muslim art existing anywhere in the world, and also of the unique Mughal architectural style that combines classic Persian and Indian features. This impressive complex of structures, crowned by its universally recognized white marble dome, represents a feat of architecture, design, and art that cannot be found anywhere else in the world.")
+}
+function seven() {
+  window.alert("The historical city of Petra is Jordan’s most recognizable symbol, and also their most visited tourist attraction. This ancient city, carved and built from the living rock possibly as early as 300 B.C., is often called the Rose City because of the pale red hue of the stone from which it was carved. Although Petra is a carefully protected and treasured site, it remained unknown in the Western world until the early 1800s, and was only designated a World Heritage Site very recently. Its inclusion as one of the New Seven Wonders has increased awareness and tourism dramatically.")
+}
